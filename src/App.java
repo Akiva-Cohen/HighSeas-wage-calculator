883fa4036e6b64c5d69326db59f234cab736bbc8
Akiva Cohen <Akivaecohen@gmail.com>
2024-12-09 21:49:05 -0500

    removed println

--- src/App.java
+++ src/App.java
@@ -2,7 +2,6 @@ import javax.swing.JFrame;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
         Window win = new Window();
         win.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         win.setVisible(true);
